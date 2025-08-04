382df3d862936f802e4576a37b39f7efd2dc8f39
dmbecker1 <danielle.becker@csuglobal.edu>
2025-08-03 21:37:52 -0600

    Create src/main.java file with Main class implementation

---
+++ src/main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        CounterUp counterUp = new CounterUp();
+        CounterDown counterDown = new CounterDown();
+
+        Thread t1 = new Thread(counterUp);
+        Thread t2 = new Thread(counterDown);
+
+        t1.start();
+        try {
+            t1.join(); // Wait for counterUp to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t2.start(); // Start counterDown after counterUp completes
+    }
+}
